30f68b4adede041b28e4dd0b20cde5b0dac8923c
AlyssaTifara <tissaalyssa@gmail.com>
2024-10-08 21:45:15 +0700

    pk1

---
+++ Staff.java
@@ -0,0 +1,31 @@
+public class Staff extends Karyawan {
+    private int lembur;
+    private double gajiLembur;
+
+    public void setLembur(int lembur){
+        this.lembur = lembur;
+    }
+    public int getLembur(){
+        return lembur;
+    }
+    public void setGajiLembur(double gajiLembur){
+        this.gajiLembur = gajiLembur;
+    }
+    public double getGajiLembur(){
+        return gajiLembur;
+    }
+    public double getGaji(){
+        return super.getGaji() + lembur * gajiLembur;
+    }
+
+    public void lihatInfo(){
+        System.out.println("-----------------------------");
+        System.out.println("NIP             : " + super.getNip());
+        System.out.println("Nama            : " + super.getNama());
+        System.out.println("Golongan        : " + super.getGolongan());
+        System.out.println("Jumlah Lembur   : " + this.getLembur());
+        System.out.println("Gaji Lembur     : " + this.getGajiLembur());
+        System.out.printf("Gaji      : %.0f\n", this.getGaji());
+        System.out.println("-----------------------------");
+    }
+}
