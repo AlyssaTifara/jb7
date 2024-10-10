88f9283dd3e9332d4b67f27b0d5532d6d0929db3
AlyssaTifara <tissaalyssa@gmail.com>
2024-10-10 10:08:35 +0700

    lat

--- PerkalianKu.java
+++ PerkalianKu.java
@@ -3,13 +3,13 @@ public class PerkalianKu {
         System.out.println("Hasil Perkalian 2 Bilangan: " + a * b);
     }
 
-    void perkalian(int a, int b, int c){
-        System.out.println("Hasil Perkalian 3 Bilangan: " + a * b * c);
+    void perkalian(double a, double b){
+        System.out.println("Hasil Perkalian 2 Bilangan: " + a * b);
     }
 
     public static void main(String[] args) {
         PerkalianKu objek = new PerkalianKu();
         objek.perkalian(25, 43);
-        objek.perkalian(34, 23, 56);
+        objek.perkalian(34.56, 23.7);
     }
 }
